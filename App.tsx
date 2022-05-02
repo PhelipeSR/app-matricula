c22153fd0db6aba48a19abdee674bfd312825dd4
Phelipe SR <phelipeuni@gmail.com>
2022-05-01 22:01:03 -0300

    Criando tela de introdução

--- App.tsx
+++ App.tsx
@@ -1,22 +1,35 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaView, StyleSheet, View, Text, Image, Button } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import Intro from './screens/intro';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [showRealApp, setShowRealApp] = useState(false);
+
+
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <>
+        {showRealApp ? (
+            <SafeAreaProvider>
+              <Navigation colorScheme={colorScheme} />
+              <StatusBar />
+            </SafeAreaProvider>
+        ) : (
+          <Intro showRealAppFunc={setShowRealApp} />
+        )}
+      </>
     );
   }
 }
---
+++ screens/intro.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+interface Props {
+  showRealAppFunc: React.Dispatch<boolean>;
+}
+
+interface SlidesProps {
+  key: string;
+  text: string;
+  title: string; 
+  icon: 'book' | 'search' | 'calendar-outline';
+  backgroundColor: string;
+}
+
+interface SlidesPropsItems {
+  item: SlidesProps
+}
+
+
+export default function Intro({ showRealAppFunc }: Props) {
+
+  const onDone = () => {
+    showRealAppFunc(true);
+  };
+
+  const onSkip = () => {
+    showRealAppFunc(true);
+  };
+  
+  const slides:SlidesProps[] = [
+    {
+      key: 's1',
+      text: 'Complete seu pedido de ajuste da matrícula',
+      title: 'Matrícula',
+      backgroundColor: '#98c000',
+      icon: 'book',
+    },
+    {
+      key: 's2',
+      title: 'Lista de Oferta',
+      text: 'Veja todas as matérias ofestadas no semestre',
+      backgroundColor: '#0068b4',
+      icon: 'search',
+    },
+    {
+      key: 's3',
+      title: 'Histórico',
+      text: 'Visualize o histórico de processamento',
+      backgroundColor: '#00a0a7',
+      icon: 'calendar-outline',
+    },
+  ];
+
+  const RenderItem = ({ item } : SlidesPropsItems) => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: item.backgroundColor,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          paddingBottom: 100,
+        }}>
+        <Text style={styles.introTitleStyle}>{item.title}</Text>
+        <Ionicons name={item.icon} size={120} color="white" />
+        <Text style={styles.introTextStyle}>{item.text}</Text>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      padding: 10,
+      justifyContent: 'center',
+    },
+    titleStyle: {
+      padding: 10,
+      textAlign: 'center',
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    paragraphStyle: {
+      padding: 20,
+      textAlign: 'center',
+      fontSize: 16,
+    },
+    introTextStyle: {
+      fontSize: 18,
+      color: 'white',
+      textAlign: 'center',
+      paddingVertical: 30,
+    },
+    introTitleStyle: {
+      fontSize: 25,
+      color: 'white',
+      textAlign: 'center',
+      marginBottom: 16,
+      fontWeight: 'bold',
+    },
+  });
+
+  return (
+    <AppIntroSlider
+      data={slides}
+      renderItem={RenderItem}
+      onDone={onDone}
+      showSkipButton={true}
+      onSkip={onSkip}
+    />
+  );
+};
