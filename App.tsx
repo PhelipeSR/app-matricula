789688eb8ac5ed44245a8f491a006922679e8742
Phelipe SR <phelipeuni@gmail.com>
2022-05-03 00:18:17 -0300

    Estilizando tela de login

--- App.tsx
+++ App.tsx
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
 import Routes from './navigation';
 import { AuthProvider } from './contexts/auth';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {return null}
 
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+    },
+  };
+
   return (
     <AuthProvider>
-      <SafeAreaProvider>
-        <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <Routes />
-        </NavigationContainer>
-        <StatusBar />
-      </SafeAreaProvider>
+        <PaperProvider theme={theme}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Routes />
+          </NavigationContainer>
+          <StatusBar />
+        </SafeAreaProvider>
+      </PaperProvider>
     </AuthProvider>
   );
 }
--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,27 +1,98 @@
-import { useContext } from 'react';
-import { Button, StyleSheet } from 'react-native';
-import { Text, View } from '../components/Themed';
+import { useContext, useState } from 'react';
+import { StyleSheet, View, Keyboard, Image, ImageBackground} from 'react-native';
 import authContext from '../contexts/auth';
+import { TextInput, Button, HelperText, Snackbar } from 'react-native-paper';
 
 
 export default function SignInScreen() {
-  const { signed, SignIn } = useContext(authContext);
+  const { SignIn } = useContext(authContext);
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorUser, setErrorUser] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+  const [errorSignIn, setErrorSignIn] = useState<string | undefined>('');
+  const [visible, setVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   async function handleSignIn() {
-    const {error, msg} = await SignIn('123456789', '123');
+    
+    setErrorUser(!user);
+    setErrorPassword(!password)
+
+    if (user && password) {
+      setLoading(true);
+      Keyboard.dismiss();
+
+      const {error, msg} = await SignIn(user, password);
+
+      if (error) {
+        setVisible(true);
+        setErrorSignIn(msg);
+        setLoading(false);
+      }
+    }
+  }
+
+  function handleUserChangeText(text: string) {
+    setErrorUser(!text);
+    setUser(text);
+  }
+
+  function handlePasswordChangeText(text: string) {
+    setErrorPassword(!text);
+    setPassword(text);
+  }
+
+  function onDismissSnackBar() {
+    setVisible(false);
   }
 
   return (
-    <View style={styles.container}>
-      <Button title='Logar' onPress={handleSignIn}></Button>
-    </View>
+    <ImageBackground style={{flex: 1, justifyContent: 'center'}} source={require('../assets/images/background.png')} resizeMode="cover">
+      <View style={styles.container}>
+        <Snackbar wrapperStyle={{marginHorizontal:20}} style={{marginHorizontal:0, backgroundColor: '#dc3545'}} visible={visible} onDismiss={onDismissSnackBar} duration={3000}>
+          {errorSignIn}
+        </Snackbar>
+        <View style={{ alignItems: 'center', height: 220}}>
+          <Image resizeMode='center' style={{paddingVertical: 100, paddingHorizontal: 10, height: 'auto'}} source={require('../assets/images/logo.png')} />
+        </View>
+        <TextInput
+          mode="outlined"
+          label="Matrícula"
+          value={user}
+          onChangeText={handleUserChangeText}
+          style={styles.input}
+          left={<TextInput.Icon name="account" />}
+        />
+        <HelperText type="error" visible={errorUser}>Matrícula inválida</HelperText>
+
+        <TextInput
+          mode="outlined"
+          label="Senha"
+          value={password}
+          onChangeText={handlePasswordChangeText}
+          secureTextEntry
+          style={styles.input}
+          left={<TextInput.Icon name="eye" />}
+        />
+        <HelperText type="error" visible={errorPassword}>Senha inválida</HelperText>
+
+        <Button style={styles.button} loading={loading} disabled={loading} mode='contained' onPress={handleSignIn}>Entrar</Button>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    paddingHorizontal: 20,
     justifyContent: 'center',
   },
+  button: {
+    marginTop: 30,
+  },
+  input: {
+    // marginTop: 20,
+  }
 });
